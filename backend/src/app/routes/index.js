a1242efea16ec561504e712dbca96d14b3f3e248
Iván Gómez Yaury <ivan.gomez@trocafone.com>
2020-06-13 20:51:18 -0300

    Adding cities' routes

--- backend/src/app/routes/index.js
+++ backend/src/app/routes/index.js
@@ -1,9 +1,10 @@
 const { healthCheck } = require('../controllers/health_check');
 
 const usersRoute = require('./users');
+const citiesRoute = require('./cities');
 
 exports.init = app => {
   app.get('/health', healthCheck);
-  const routes = [usersRoute];
+  const routes = [usersRoute, citiesRoute];
   routes.forEach(route => route.init(app));
 };
