16302c0e9efe1831cd4caae5b00855b5a1ec4a45
Iván Gómez Yaury <ivan.gomez@trocafone.com>
2020-06-14 20:16:46 -0300

    updating users controller

--- backend/src/app/controllers/users.js
+++ backend/src/app/controllers/users.js
@@ -1,6 +1,13 @@
-const { getUsers } = require('../services/users');
-const { getUsersMapper } = require('../mappers/users');
+const { getUsers, createUser, getUserById, updateUser, deleteUser } = require('../services/users');
+const {
+  getUsersMapper,
+  createUserMapper,
+  getUserMapper,
+  updateUserMapper,
+  deleteUserMapper
+} = require('../mappers/users');
 const { paginateResponse } = require('../serializers/paginations');
+const { getUserSerializer } = require('../serializers/users');
 
 exports.getUsers = (req, res, next) => {
   const filters = getUsersMapper(req);
@@ -8,3 +15,25 @@ exports.getUsers = (req, res, next) => {
     .then(({ count, rows }) => res.status(200).send(paginateResponse({ ...filters, count, data: rows })))
     .catch(next);
 };
+
+exports.createUser = (req, res, next) =>
+  createUser(createUserMapper(req))
+    .then(() => res.status(201).end())
+    .catch(next);
+
+exports.getUser = (req, res, next) =>
+  getUserById(getUserMapper(req))
+    .then(user => res.status(200).send(getUserSerializer(user)))
+    .catch(next);
+
+exports.updateUser = (req, res, next) => {
+  const newAttributes = updateUserMapper(req);
+  return updateUser(newAttributes)
+    .then(() => res.status(204).end())
+    .catch(next);
+};
+
+exports.deleteUser = (req, res, next) =>
+  deleteUser(deleteUserMapper(req))
+    .then(() => res.status(204).end())
+    .catch(next);
