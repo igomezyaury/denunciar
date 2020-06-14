05eecbd8ccc5f3f25146ad49784201537e28edcd
Iván Gómez Yaury <ivan.gomez@trocafone.com>
2020-06-14 20:13:07 -0300

    Adding mapper for users

--- backend/src/app/mappers/users.js
+++ backend/src/app/mappers/users.js
@@ -1,5 +1,45 @@
-const { pagination } = require('./common');
+const { pagination, addCommonProperties, OPTIONAL_ACTIVE, ACTIVE, ID } = require('./common');
+const {
+  USER_ROLES: { NORMAL }
+} = require('../utils/constants');
 
 exports.getUsersMapper = req => ({
   ...pagination(req)
 });
+
+exports.createUserMapper = req =>
+  addCommonProperties(
+    {
+      username: req.body.username,
+      firstName: req.body.first_name,
+      lastName: req.body.last_name,
+      identificationCode: req.body.identification_code,
+      identificationTypeId: req.body.identification_type_id,
+      birthDate: req.body.birth_date,
+      email: req.body.email,
+      password: req.body.password,
+      rol: req.body.rol || NORMAL
+    },
+    req,
+    [OPTIONAL_ACTIVE]
+  );
+
+exports.getUserMapper = req => addCommonProperties({}, req, [ID]);
+
+exports.updateUserMapper = req =>
+  addCommonProperties(
+    {
+      username: req.body.username,
+      firstName: req.body.first_name,
+      lastName: req.body.last_name,
+      identificationCode: req.body.identification_code,
+      identificationTypeId: req.body.identification_type_id,
+      birthDate: req.body.birth_date,
+      email: req.body.email,
+      rol: req.body.rol
+    },
+    req,
+    [ACTIVE, ID]
+  );
+
+exports.deleteUserMapper = req => addCommonProperties({}, req, [ID]);
