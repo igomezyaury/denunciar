66331d6cfeb432400637073a92073164914932f5
Matías Camerucci <mcamerucci@kovix-group.com>
2020-08-01 18:59:34 -0300

    Add user and id-type models

---
+++ frontend/src/app/models/identification-type.ts
@@ -0,0 +1,6 @@
+export const identificationTypes = [
+    { id:'1', name: 'DNI'},
+    { id:'2', name: 'Cédula civil'},
+    { id:'3', name: 'Libreta de enrolamiento'},
+    { id:'4', name: 'Libreta cívica'}
+]
