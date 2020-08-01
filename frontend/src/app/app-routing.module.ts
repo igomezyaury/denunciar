18c2922eda42a65b1a0a9b91a96b9084a7b91cf2
Matías Camerucci <mcamerucci@kovix-group.com>
2020-08-01 19:01:09 -0300

    Add routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,40 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from './authentication/auth.guard';
+import { LayoutComponent } from './layout/layout/layout.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:        '',
+    component: LayoutComponent,
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('./home/home-routing.module').then(
+            mod => mod.HomeRoutingModule
+          )
+      },
+      {
+        path: 'users',
+        loadChildren: () =>
+          import('./user/user-routing.module').then(
+            mod => mod.UserRoutingModule
+          )
+      },
+    ]
+  },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./authentication/authentication-routing.module').then(
+        mod => mod.AuthenticationRoutingModule
+      )
+  }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,6 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { TokenInterceptor } from './authentication/token.interceptor';
+import { AuthenticationModule } from './authentication/authentication.module';
+import { HomeModule } from './home/home.module';
+import { AuthenticationService } from './authentication/authentication.service';
+import { UserModule } from './user/user.module';
+import { UserService } from './user/user.service';
+import { LayoutModule } from './layout/layout.module';
+import { DatePipe } from '@angular/common';
+
 
 @NgModule({
   declarations: [
@@ -10,9 +20,23 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    AuthenticationModule,
+    LayoutModule,
+    UserModule,
+    HomeModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    AuthenticationService,
+    UserService,
+    DatePipe,
+    { 
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true 
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
