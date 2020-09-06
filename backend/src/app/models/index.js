293e3ebc10ab8a184e0d98cab6185211bdd8a837
Iván Gómez Yaury <igomezyaury@gmail.com>
2020-09-05 21:26:53 -0300

    adding pw

--- backend/src/app/models/index.js
+++ backend/src/app/models/index.js
@@ -4,7 +4,7 @@ const { Sequelize } = require('sequelize');
 const dbConfig = require('../../config').common.database;
 
 // const connectionString = `${dbConfig.dialect}://${dbConfig.username}:${dbConfig.password}@${dbConfig.host}:${dbConfig.port}/${dbConfig.database}`;
-const connectionString = `postgresql://doadmin:show-password@db-postgresql-nyc1-72417-do-user-7982741-0.a.db.ondigitalocean.com:25060/defaultdb?sslmode=require`;
+const connectionString = `postgresql://doadmin:hlpf9lre6983cr9y@db-postgresql-nyc1-72417-do-user-7982741-0.a.db.ondigitalocean.com:25060/defaultdb?sslmode=require`;
 const basename = path.basename(__filename);
 const db = {};
 const sequelize = new Sequelize(connectionString, {
