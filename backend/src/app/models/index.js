e2ea4322d27da0e67bfd7f3b68aa2bccea357888
Iván Gómez Yaury <igomezyaury@gmail.com>
2020-09-05 21:10:17 -0300

    Adding ssl option to connection string

--- backend/src/app/models/index.js
+++ backend/src/app/models/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const { Sequelize } = require('sequelize');
 const dbConfig = require('../../config').common.database;
 
-const connectionString = `${dbConfig.dialect}://${dbConfig.username}:${dbConfig.password}@${dbConfig.host}:${dbConfig.port}/${dbConfig.database}`;
+const connectionString = `${dbConfig.dialect}://${dbConfig.username}:${dbConfig.password}@${dbConfig.host}:${dbConfig.port}/${dbConfig.database}?ssl=true`;
 const basename = path.basename(__filename);
 const db = {};
 const sequelize = new Sequelize(connectionString, {
